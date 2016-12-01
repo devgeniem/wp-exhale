2a84fe9564b86b14b56708e3e0e9a6b7ddb473fe
Onni Hakala <onni@keksi.io>
2016-12-01 15:00:26 +0200

    Version bump

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WP Exhale
  * Description: Developer friendly exporter for WordPress
- * Version: 0.1.4
+ * Version: 0.1.5
  * Author: Onni Hakala / Geniem Oy
  * Author URI: http://geniem.com
  * License: GPLv2
